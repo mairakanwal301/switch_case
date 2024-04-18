c24f5bcd064a17abfa57bc6115ef25e5407c4b66
mairarafiq301 <mairarafiq301@gmail.com>
2024-04-18 11:05:24 -0700

    first commit

---
+++ app.ts
@@ -0,0 +1,29 @@
+let weather = "sunny";
+switch (weather){
+    case "rainy" :
+        console.log( "you wear raincoat ")
+
+        break;
+
+    case "sunny" :
+        console.log("you wear sunglsses")
+    
+       break;
+
+
+    case "cold":
+    console.log ("wear warm cloths")
+
+     break;
+
+    case "cloudy" :
+    console.log("take umbrella")
+
+    break;
+
+    default: 
+    console.log("check weather forcast")
+
+}
+
+
